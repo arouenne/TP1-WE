c1ad0b2d5bab6978013cbd209df8078876804f02
Arwen <arwen.toulemont@orange.fr>
2024-09-20 09:31:00 +0200

    Séance TP1

--- interaction.js
+++ interaction.js
@@ -3,10 +3,44 @@
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
 	// Définir ici les attributs de la 'classe'
+  this.position_x1 = 0;
+  this.position_x2 = 0;
+  this.position_y1 = 0;
+  this.position_y2 = 0;
+  this.isPressed = false;
 
 	// Developper les 3 fonctions gérant les événements
 
+  this.mouseDown = function(evt) {
+    var position = getMousePosition(canvas, evt)
+    this.x = position.x
+    this.y = position.y
+    this.isPressed = true
+    console.log("Pressed on : X "+ this.x +", Y "+ this.y)
+  }.bind(DnD)
+
+  this.mouseMove = function(evt) {
+    var position = getMousePosition(canvas, evt)
+    this.x = position.x
+    this.y = position.y
+    console.log("Moving on : X "+ this.x +", Y "+ this.y)
+  }.bind(DnD)
+
+  this.mouseUp = function(evt) {
+    if(this.pressed) {
+      var position = getMousePosition(canvas, evt)
+      this.x = position.x
+      this.y = position.y
+      this.isPressed = false
+    }
+    console.log("Released X "+ this.x +", Y "+ this.y)
+  }.bind(DnD)
+
 	// Associer les fonctions précédentes aux évènements du canvas.
+  canvas.addEventListener('mousedown', this.mouseDown, false);
+  canvas.addEventListener('mousemove', this.mouseMove, false);
+  canvas.addEventListener('mouseup', this.mouseUp, false);
+
 };
 
 
--- model.js
+++ model.js
@@ -1,3 +1,31 @@
 
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
+
+function Drawing() {
+    this.drawing = new Array();
+};
+
+function Shape() {
+    this.color = null;
+    this.thickness = null;
+}
+
+function Rectangle(){
+    Shape.call(this)
+    this.x = 0;
+    this.y = 0;
+    this.height = 0;
+    this.width = 0;
+};
+
+function Line(){
+    Shape.call(this)
+    this.x_start = 0;
+    this.y_start = 0;
+    this.x_final = 0;
+    this.y_final = 0;
+}
+  
+  
+  
